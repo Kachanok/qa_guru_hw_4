280caead5b0c2d3f2a02306d9795dc12f4be87bf
AnastasiaF <kachanav1009@gmail.com>
2023-11-05 18:03:58 +0300

    added build.gradle/.gitignore + src\test\java\github\SelenideRepositorySearch.java

---
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -0,0 +1,57 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideRepositorySearch {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/"; // открыть главную страницу
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+
+    @Test
+    void shouldFindSelenideRepository() {
+
+        open("selenide/selenide");
+        //перейти в раздел wiki проекта
+
+        $("#wiki-tab").click();
+
+        //убедиться, что в списке страниц (Pages) есть страница SoftAssertions
+
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+
+        //открыть страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+
+        $("[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $("#js-repo-pjax-container").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }"""));
+
+        System.out.println("Тест пройден успешно.");
+
+        sleep(5000);
+
+
+    }
+}
